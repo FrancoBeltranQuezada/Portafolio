9bd82c3de23f686fb950ff6f901fb1b5446d10b9
miguelrubioh <56160047+miguelrubioh@users.noreply.github.com>
2020-05-14 20:17:36 -0400

    modelo proveedor

--- proveedor/models.py
+++ proveedor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class prov(models.Model):
+    id_prov = models.AutoField(primary_key=True)
+    nombre_proveedor= models.CharField(max_length=30)
+    
