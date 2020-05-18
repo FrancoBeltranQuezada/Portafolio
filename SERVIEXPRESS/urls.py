67285cbdad789ba378535b6a467e72d752161f55
miguelrubioh <56160047+miguelrubioh@users.noreply.github.com>
2020-05-18 18:41:59 -0400

    prueba 2 urls

--- SERVIEXPRESS/urls.py
+++ SERVIEXPRESS/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('producto/',include('producto.urls')),    
+    path('producto/',include('producto.urls')),
+    path('proveedor/',include('proveedor.urls'))
+     
 
 ]
--- producto/urls.py
+++ producto/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='producto-home'),
+    path('proveedor/',views.home, name='producto-home'),
     path('about/',views.home, name='producto-about'),
 ]
