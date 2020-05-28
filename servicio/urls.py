7be2b809597d14a77f1f0e6b6e4bca5d9ff8ffe4
DemianVelisCanulao <DemianVelisCanulao@users.noreply.github.com>
2020-05-27 21:18:47 -0400

    Ingresar y Modificar Servicio

--- servicio/urls.py
+++ servicio/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home, name='servicio-home'),
+    #path('home/',views.home, name='servicio-home'),
     path('about/',views.about, name='servicio-about'),
     path('registrar/',views.registrar_servicio, name= 'registrar-servicio'),
-    path('actualizar_servicio/<str:pk>', views.actualizar_servicio, name='actualizar-servicio'),
+    path('actualizar_servicio/<int:id_servicio>', views.actualizar_servicio, name='actualizar-servicio'),
+    path('home/',views.listar_servicio, name= 'listar-servicio'),
 ]
--- servicio/views.py
+++ servicio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ServicioForm
+from servicio.models import Servicio
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -26,9 +28,22 @@ def registrar_servicio (request):
     return render (request,'servicio/registrar.html', data)
 
 
-def actualizar_servicio (request, pk):
-    data = {
-        'form' : ServicioForm()
-    }       
+def actualizar_servicio(request, id_servicio):
+    servicio = Servicio.objects.get(id_servicio=id_servicio)
 
-    return render (request, 'servicio/registrar.html', data)
+    if request.method == 'GET':
+        form = ServicioForm(instance = servicio)
+    else:
+        form = ServicioForm(request.POST, instance = servicio)
+        if form.is_valid():
+            form.save()
+           
+            return redirect ('listar-servicio')   
+    contexto ={'form' : form}         
+    return render (request, 'servicio/registrar.html', contexto)
+
+
+def listar_servicio(request):
+    servicio = Servicio.objects.all()
+    contexto ={'servicios' : servicio}   
+    return render (request, 'servicio/home.html', contexto)
