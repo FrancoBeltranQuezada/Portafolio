2ab56281fc0b2f9f4fbcd8e14073158bb0429d71
DemianVelisCanulao <DemianVelisCanulao@users.noreply.github.com>
2020-05-28 21:38:56 -0400

    Boton eliminar

--- servicio/urls.py
+++ servicio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('registrar/',views.registrar_servicio, name= 'registrar-servicio'),
     path('actualizar_servicio/<int:id_servicio>', views.actualizar_servicio, name='actualizar-servicio'),
     path('home/',views.listar_servicio, name= 'listar-servicio'),
+    path('eliminar/<int:id_servicio>', views.eliminar_servicio, name='eliminar-servicio'),
 ]
