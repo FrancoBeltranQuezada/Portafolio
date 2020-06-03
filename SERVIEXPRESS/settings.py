6fa1af8129acfd389d64f1c3782f182f5f6e0385
DemianVelisCanulao <DemianVelisCanulao@users.noreply.github.com>
2020-06-02 20:30:30 -0400

    update home

--- SERVIEXPRESS/settings.py
+++ SERVIEXPRESS/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'SERVIEXPRESS.wsgi.application'
 DATABASES = {
     'default': {
     'ENGINE':   'django.db.backends.oracle',
-    'NAME':     'localhost:1522/XE',
+    'NAME':     'localhost:1521/XE',
     'USER':     'C##DJANGO',
     'PASSWORD': 'django',
     
