e10d660b28c39ac6f945d699e59d00f80992ac0e
DemianVelisCanulao <DemianVelisCanulao@users.noreply.github.com>
2020-06-05 21:04:53 -0400

    login_requiered a las app restante

--- proveedor/urls.py
+++ proveedor/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('',views.index, name='proveedor-home'),
-    path('aboutt/',views.aboutt, name='proveedor-about'),
-    path('crear_proveedor/',views.crear_proveedor,name='crear_proveedor'),
-    path('editar_proveedor/<str:id_prov>/', views.editar_proveedor, name='editar_proveedor'),
-    path('eliminar_proveedor/<str:id_prov>/', views.eliminar_proveedor, name='eliminar_proveedor'),
-    path('eliminar/<int:id_prov>', views.eliminar_proveedor, name='eliminar_proveedor'),
+    path('',login_required(views.index), name='proveedor-home'),
+    path('aboutt/',login_required(views.aboutt), name='proveedor-about'),
+    path('crear_proveedor/',login_required(views.crear_proveedor),name='crear_proveedor'),
+    path('editar_proveedor/<str:id_prov>/', login_required(views.editar_proveedor), name='editar_proveedor'),
+    path('eliminar_proveedor/<str:id_prov>/', login_required(views.eliminar_proveedor), name='eliminar_proveedor'),
+    path('eliminar/<int:id_prov>', login_required(views.eliminar_proveedor), name='eliminar_proveedor'),
   
 ]
