038239e37c7340e1e9a38a078ddd003de80ead53
Franco <francobeltran@gmail.com>
2020-07-12 19:59:20 -0400

    Inicio carrito boleta

--- boleta/models.py
+++ boleta/models.py
@@ -9,35 +9,51 @@ from datetime import datetime
 # Create your models here.
 
 
+class DetalleBoleta(models.Model):
+    servicio = models.OneToOneField(Servicio,on_delete=models.SET_NULL,null=True)
+    fecha_agregada = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.servicio.name
+    
+
+# class DetalleBoleta (models.Model):
+#     vunitario = models.PositiveIntegerField(verbose_name='Precio')
+#     desc_prod = models.CharField(max_length=30, verbose_name='Descripción producto')
+#     cantidad = models.PositiveIntegerField(null=False)
+#     totallinea = models.IntegerField()
+#     boleta_id_boleta = models.ForeignKey(Boleta, on_delete = models.SET_NULL, null = True)
+#     servicio = models.ForeignKey(Servicio, on_delete = models.SET_NULL, null = True)
+#     producto = models.ForeignKey(Producto, on_delete = models.CASCADE, null = True)
+
+
+#     def __str__(self):
+#         return self.boleta_id_boleta
+
+
+
 class Boleta (models.Model):
     id_boleta = models.IntegerField(primary_key=True, unique=True)
     fecha = models.DateField(default = datetime.now, verbose_name='Fecha de boleta')
-    hora = models.TimeField()
-    total = models.PositiveIntegerField()
     usuario = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    servicios = models.ManyToManyField(DetalleBoleta)
     #id_forma_pago = models.ForeignKey('', on_delete = models.SET_NULL)
     #id_rol = models.ForeignKey('', on_delete = models.SET_NULL)
+
+
+    def get_boleta_servicios(self):
+        return self.servicios.all()
+
+    def get_boleta_total(self):
+        return sum([servicio.servicio.valor_servicio for servicio in self.servicios.all()])
+
+    
     
     def __str__(self):
-        return str(self.id_boleta)
+        return '{0} - {1}'.format(self.usuario,self.id_boleta)
 
     
 
     
-
-
-class DetalleBoleta (models.Model):
-    vunitario = models.PositiveIntegerField(verbose_name='Precio')
-    desc_prod = models.CharField(max_length=30, verbose_name='Descripción producto')
-    cantidad = models.PositiveIntegerField(null=False)
-    totallinea = models.IntegerField()
-    boleta_id_boleta = models.ForeignKey(Boleta, on_delete = models.SET_NULL, null = True)
-    servicio = models.ForeignKey(Servicio, on_delete = models.SET_NULL, null = True)
-    producto = models.ForeignKey(Producto, on_delete = models.CASCADE, null = True)
-
-
-    def __str__(self):
-        return self.boleta_id_boleta
 
 
 
